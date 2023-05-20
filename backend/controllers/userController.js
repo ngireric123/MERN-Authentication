68458046bc3716e518bdad58c4123ae8bb68c5a0
ngireric123 <ngireric123@gmail.com>
2023-05-20 12:23:29 -0400

    creating Routers structure

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,4 +7,42 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth user" });
 });
 
-export { authUser };
+// @desc Register a new user
+// route POST /api/users
+// @access public
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "register user" });
+});
+
+// @desc Logout user
+// route POST /api/users/logout
+// @access public
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "logout user" });
+});
+
+// @desc user Profile
+// route GET /api/users/profile
+// @access private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "user Profile" });
+});
+
+// @desc Update user Profile
+// route PUT /api/users/profile
+// @access private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
