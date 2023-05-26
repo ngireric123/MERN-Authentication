2718767d48533c0ce0adbfeca709f2bde7c64df0
ngireric123 <ngireric123@gmail.com>
2023-05-25 21:35:07 -0400

    adding cookie on user end point

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 // @desc Auth user/set token
 // route POST /api/users/auth
@@ -21,7 +22,24 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("User already exists");
   }
-  res.status(200).json({ message: "register user" });
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
 });
 
 // @desc Logout user
