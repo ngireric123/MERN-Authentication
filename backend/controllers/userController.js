3392f1260dc3d61addf2d48b604661b605c58c4f
ngireric123 <ngireric123@gmail.com>
2023-05-25 23:38:16 -0400

    adding Update User Profile functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -86,7 +86,27 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @access private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update User Profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404).json;
+    throw new Error("User not found");
+  }
 });
 
 export {
